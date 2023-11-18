0b98140196ec856052a188c7325b11ac2d28a8e0
TithiRojivadiya <trojivadiya@gmail.com>
2023-11-18 18:52:49 +0530

    Change Font size and color

--- SnakeGame.py
+++ SnakeGame.py
@@ -31,11 +31,18 @@ bgimg3 = pygame.transform.scale(bgimg3, (screen_width, screen_height)).convert_a
 pygame.display.set_caption("SnakeGameWithKeira")
 pygame.display.update()
 clock = pygame.time.Clock()
-font = pygame.font.SysFont(None, 55)
+# font = pygame.font.SysFont(None, 55)
+
+custom_font = pygame.font.Font('gomarice_no_continue.ttf', 55)
+custom_font2 = pygame.font.Font('gomarice_no_continue.ttf', 30)
 
 def text_screen(text,color,x,y):
-    screen_text = font.render(text,True,color)
+    screen_text = custom_font.render(text, True, color)
     gameWindow.blit(screen_text, (x,y))
+
+def text_screen2(text,color,x,y):
+    screen_text2 = custom_font2.render(text, True, color)
+    gameWindow.blit(screen_text2, (x,y))
 
 def plot_snake(gameWindow, color, snk_list, snake_size):
     for x,y in snk_list:
@@ -49,8 +56,9 @@ def welcome():              #Welcome screen
 
         gameWindow.fill((130,99,150))#purple color
         gameWindow.blit(bgimg2,(0,0))
-        text_screen("Welcome to Snakes" ,black,250,250)
-        text_screen("Press Space Bar to Play" ,black,220,300)
+        text_screen2("Snakes Game by Tithi" ,black,5,5)
+        text_screen("Welcome to Snakes Game..." ,black,20,420)
+        text_screen("Press Space Bar to Play" ,black,270,470)
         for event in pygame.event.get() :
             if event.type == pygame.QUIT:
                 exit_game = True
@@ -97,7 +105,10 @@ def gameloop() :
                 f.write(str(highscore))
             gameWindow.fill(white)
             gameWindow.blit(bgimg3,(0,0))
-            text_screen("Game Over ! Please Enter to Continue",red,100,250)
+            text_screen("Game Over !!!",black,300,150)
+            text_screen("Press Enter to Continue...",black,150,450)
+            text_screen("Score : "+ str(score),black,550,280)
+            text_screen("Highscore : "+ str(highscore),black,20,280)
 
             for event in pygame.event.get() :
                 
@@ -150,7 +161,9 @@ def gameloop() :
 
             gameWindow.fill(white)
             gameWindow.blit(bgimg,(0,0))
-            text_screen("Score : " + str(score) + " Highscore : "+str(highscore),red,5,5)
+            text_screen2("Score : " + str(score) ,(255,255,0),5,5)
+            text_screen2("Highscore : " + str(highscore) ,(255,255,0),690,5)
+    
             pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
 
             head = []
